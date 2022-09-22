34bdeeca4acc4f9f1ff69a323de78c9a51c49c26
kunal018 <f20190229@pilani.bits-pilani.ac.in>
2022-09-22 03:35:37 -0700

    First Commit

--- Learning/Hello_world.go
+++ Learning/Hello_world.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		fmt.Fprintf(w, "Hello World !!")
 	})
 	http.ListenAndServe(":9000", nil)
 }
